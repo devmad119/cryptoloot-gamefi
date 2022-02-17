c2ca7746afba11dd0f777350a59a1063748582fc
devilhacker0119 <devilhacker0119@gmail.com>
2022-02-17 23:16:48 +0800

    fix staking page and some features

--- src/components/Breadcrumb/Breadcrumb.js
+++ src/components/Breadcrumb/Breadcrumb.js
@@ -1,27 +1,27 @@
 import React, { Component } from 'react';
 
 class Breadcrumb extends Component {
-    render() {
-        return (
-            <section className="breadcrumb-area overlay-dark d-flex align-items-center">
-                <div className="container">
-                    <div className="row">
-                        <div className="col-12">
-                            {/* Breamcrumb Content */}
-                            <div className="breadcrumb-content text-center">
-                                <h2 className="m-0">{this.props.title}</h2>
-                                {/* <ol className="breadcrumb d-flex justify-content-center">
+  render() {
+    return (
+      <section className="breadcrumb-area overlay-dark d-flex align-items-center">
+        <div className="container">
+          <div className="row">
+            <div className="col-12">
+              {/* Breamcrumb Content */}
+              <div className="breadcrumb-content text-center">
+                <h2 className="m-0">{this.props.title}</h2>
+                {/* <ol className="breadcrumb d-flex justify-content-center">
                                     <li className="breadcrumb-item"><a href="/">Home</a></li>
                                     <li className="breadcrumb-item"><a href="#">{this.props.subpage}</a></li>
                                     <li className="breadcrumb-item active">{this.props.page}</li>
                                 </ol> */}
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </section>
-        );
-    }
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    );
+  }
 }
 
 export default Breadcrumb;
